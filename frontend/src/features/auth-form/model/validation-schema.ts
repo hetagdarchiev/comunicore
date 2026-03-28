6cf261079516003854d8d0f2f2309789fa43fa74
Hamelonov <leonhamelonovich@gmail.com>
2026-03-28 22:21:49 +0500

    get token and save to localStorage

--- frontend/src/features/auth-form/model/validation-schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const loginSchema = z.object({
-  login: z.string().min(1, 'Введите почту'),
-  password: z.string().min(1, 'Введите пароль'),
-});
-
-export type LoginSchema = z.infer<typeof loginSchema>;
