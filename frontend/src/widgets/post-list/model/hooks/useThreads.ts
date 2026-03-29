c847561d80e3db0ad970c3fca0c1440428f6212a
Pro100Simpleton <179834483+hetagdarchiev@users.noreply.github.com.>
2026-03-29 15:37:01 +0300

    Заполнил хук UseThreads.ts

---
+++ frontend/src/widgets/post-list/model/hooks/useThreads.ts
@@ -0,0 +1,19 @@
+'use client';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { getThreads } from '../api/getThreads';
+
+export function useThreads(limit: number) {
+  return useInfiniteQuery({
+    queryKey: ['threads'],
+    initialPageParam: 1,
+
+    queryFn: ({ pageParam }) => getThreads(pageParam, limit),
+
+    getNextPageParam: (lastPage, allPages) => {
+      if (!lastPage.have_next) return undefined;
+      return allPages.length + 1;
+    },
+  });
+}
