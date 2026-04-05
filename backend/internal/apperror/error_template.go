96e5245db2891b359871c08fd24bdbdc62ac4319
Alex Syrnikov <alex19srv@gmail.com>
2026-04-05 16:45:58 +0300

    changed error handling schema
    now text description go from fmt.Errof("text %w\n caused %w", NewAppError(), err)
    and typed errors for errors.Is, errors.As comes from %w

--- backend/internal/apperror/error_template.go
+++ backend/internal/apperror/error_template.go
@@ -3,59 +3,32 @@
 
 package apperror
 
-import (
-	"strings"
-)
-
-type ErrorChain struct {
-	opName string // current operation produced error
-	cause  error  // original error (can be ErrorChain or any other error)
-	value  error  // error value
-}
-
-func (b *ErrorChain) WithCause(cause error) *ErrorChain {
-	b.cause = cause
-	return b
-}
-func (b *ErrorChain) InOperation(opName string) *ErrorChain {
-	b.opName = opName
-	return b
-}
-
-// error message including all wrapped errors
-func (b *ErrorChain) Error() string {
-	errValue := b.value
-	if errValue == nil {
-		return ""
-	}
-	var res strings.Builder
-	res.Grow(4096)
-	res.WriteString(errValue.Error() + "\n")
-
-	errCause := b.cause
-	for errCause != nil {
-		switch cause := errCause.(type) {
-		case *ErrorChain:
-			res.WriteString("caused by: " + cause.value.Error() + "\n")
-			errCause = cause.cause
-			continue
-		default:
-			res.WriteString("caused by: " + errCause.Error() + "\n")
-			errCause = nil
-		}
-	}
-	return res.String()
-}
+// error handling algorythm:
+//
+//	if err != nil {
+//	    return fmt.Errorf("error: %w\n caused by: %w", NewNotUniqueError(), err)
+//	}
+//
+// here Error() with text description will go from Errorf,
+// and NewNotUniqueError() will be used to create typed error that can be checked with errors.Is or errors.As
 
 type UnspecifiedError struct {
 	msg string
 }
 
-func NewUnspecifiedError(msg string) *ErrorChain {
-	return &ErrorChain{
-		value: &UnspecifiedError{msg: msg},
-	}
+func NewUnspecifiedError(msg string) *UnspecifiedError {
+	return &UnspecifiedError{msg: msg}
 }
 func (e *UnspecifiedError) Error() string {
-	return e.msg
+	return "Unspecified error: " + e.msg
+}
+
+type NotUniqueError struct {
+}
+
+func NewNotUniqueError(name ...string) *NotUniqueError {
+	return &NotUniqueError{}
+}
+func (e *NotUniqueError) Error() string {
+	return "NotUniqueError"
 }
