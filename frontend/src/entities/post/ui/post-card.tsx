4fbcbb690b8b62000e839c4c4594cb1df737f0fb
Hamelonov <leonhamelonovich@gmail.com>
2026-04-21 20:36:34 +0500

    Исправлены типы для полчения данных threads для PostCard.tsx

--- frontend/src/entities/post/ui/post-card.tsx
+++ frontend/src/entities/post/ui/post-card.tsx
@@ -10,42 +10,42 @@ import { PostTags } from './post-tags/post-tags';
 import { PostViews } from './post-views';
 
 export const PostCard = ({
-  author_name,
+  authorName,
   avatarUrl,
-  created_at,
+  createdAt,
   title,
   children,
   tags,
   isLiked = false,
   onLike,
   stats,
-  author_id,
+  authorId,
   id,
 }: PostProps) => (
-  <article className='border-gray-ea bg-post-card mx-auto w-full max-w-5xl rounded-md border px-7.5 py-5 shadow-[2px_1px_5px_0px_#00000026]'>
-    <PostHeader
-      author_id={author_id}
-      author_name={author_name}
-      avatarUrl={avatarUrl}
-      created_at={created_at}
-    />
-
-    <PostContent id={id} title={title}>
-      {children}
-    </PostContent>
-
-    <div className='flex items-start justify-between gap-3 max-sm:flex-col sm:items-center'>
-      <PostTags tags={tags ?? []} />
-
-      <div className='text-gray-80 flex items-center gap-4'>
-        <PostViews count={stats?.views || 0} />
-        <PostCommentsLink count={stats?.comments || 0} postId={id} />
-        <PostLikeButton
-          count={stats?.likes || 0}
-          isLiked={isLiked}
-          onLike={onLike}
+    <article className='border-gray-ea bg-post-card mx-auto w-full max-w-5xl rounded-md border px-7.5 py-5 shadow-[2px_1px_5px_0px_#00000026]'>
+        <PostHeader
+            authorId={authorId}
+            authorName={authorName}
+            avatarUrl={avatarUrl}
+            createdAt={createdAt}
         />
-      </div>
-    </div>
-  </article>
+
+        <PostContent id={id} title={title}>
+            {children}
+        </PostContent>
+
+        <div className='flex items-start justify-between gap-3 max-sm:flex-col sm:items-center'>
+            <PostTags tags={tags ?? []} />
+
+            <div className='text-gray-80 flex items-center gap-4'>
+                <PostViews count={stats?.views || 0} />
+                <PostCommentsLink count={stats?.comments || 0} postId={id} />
+                <PostLikeButton
+                    count={stats?.likes || 0}
+                    isLiked={isLiked}
+                    onLike={onLike}
+                />
+            </div>
+        </div>
+    </article>
 );
--- frontend/src/widgets/post-list/PostList.tsx
+++ frontend/src/widgets/post-list/PostList.tsx
@@ -5,7 +5,6 @@ import { useLikeThread } from './model/hooks/useLikeThread';
 import { useThreads } from './model/hooks/useThreads';
 import { PostListLoader } from './ui/PostListLoader';
 import { PostListView } from './ui/PostListView';
-// import { Sorting } from './ui/sorting';
 
 export function PostList() {
   const postCount = 4;
