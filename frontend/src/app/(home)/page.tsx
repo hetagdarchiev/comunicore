39b4bc9ab90b65082dfdd182a8dcef89b50eebbe
Pro100Simpleton <179834483+hetagdarchiev@users.noreply.github.com.>
2026-04-30 02:34:21 +0300

    Ссылку на текстовый редактор из основной страницы

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,17 +1,10 @@
 'use client';
 
-import Link from 'next/link';
-
-import { useAuth } from '@/shared/hooks/useAuth';
 import { PostList } from '@/widgets/post-list';
 
 export default function Home() {
-  const { isAuthenticated } = useAuth();
   return (
     <main className='grow'>
-      <Link href={isAuthenticated ? '/editor' : '/registration'}>
-        Text editor
-      </Link>
       <PostList />
     </main>
   );
