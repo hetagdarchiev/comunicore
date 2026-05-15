f79b4b3464f751344d9d15af850b96bc15757fb7
Nazar <nazarpalamar46@gmail.com>
2026-05-15 22:18:31 +0200

    fix adaptive

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -19,7 +19,7 @@ export default function HomeLayout({
       <Header menuRef={menuRef} burgerRef={burgerRef} />
       <div
         className={clsx(
-          'relative mx-auto flex h-8/10 w-full max-w-360 flex-1 gap-x-5 px-4',
+          'relative mx-auto flex h-8/10 w-full max-w-360 flex-1 gap-x-5 overflow-x-hidden px-4',
           'lg:px-17.5',
         )}
       >
